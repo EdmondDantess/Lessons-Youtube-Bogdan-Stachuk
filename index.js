fb7471dffd3608ccd2f2a0d99ef2d0e35ed0af46
EdmondDantess <maximlavrovsky@mail.ru>
2022-05-04 18:21:37 +0300

    up

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ const personOne = {
     name: 'Bob',
     age: 21
 };
-function increasePersonAge(person) { 
+function increasePersonAge(person) {                              //Функции не меняют внешние переменные
     const updatedPerson = Object.assign({}, person);  
     updatedPerson.age += 3;
     return updatedPerson
@@ -121,3 +121,90 @@ function increasePersonAge(person) {
 const updatedPersonOne = increasePersonAge(personOne);  
 console.log(personOne.age);
 console.log(updatedPersonOne.age) */
+
+
+/*  //копируем св-ва из одного объекта в другом объекте при помощи оператора ...
+const button = {
+    width: 200,
+    text: 'buy'
+};
+const redButton = {
+    ...button,    //копируем св-ва оператором ...    если мутируем потом св-ва у объекта redButton, то у button ничего неизменится (если button нету вложенных объектов )
+    color: 'red'
+};
+console.table(redButton) */
+
+/* //Шаблонные строки
+  const hello = 'Hello my name is';
+   const greeting = `${hello} "Maksim" ${"Laurouski"}`; //выражение заключено в обратные кавычки
+  console.log(greeting) */
+
+/*   //Сокращения в стрелочных функциях
+   a => {                 //Если один параметр, то круглые скобки можно опустить
+       //Тело функции
+   }
+
+   (a, b) => a+b     //Фигурные скобки можно опустить, если тело функции состоит из одного выражения, return происходит автоматически
+   */
+
+
+/*  //Значения параметров функции по умолчанию пример 1 
+  result = (value, multiplier = 3) => value * multiplier;
+console.log(result(4,2));
+console.log(result(4));  */
+
+/* //Значения параметров функции по умолчанию пример 2 
+const newPost = (post, addedAt = Date()) =>  ({    //нужно ставить ещё круглые скобки. Date() это оператор вызова текущей даты. Значения по умолчанию вычисляются в момент вызова функции
+    ...post,
+    addedAt
+});
+const firstPost = {
+    id: 1,
+    author: 'Maksim'
+};
+newPost(firstPost);
+console.table(newPost(firstPost)) */
+
+/*
+  //Добавляем/удаляем в массивах элементы
+const myArray = [1, 2, 3, 4];
+console.log(myArray);
+myArray[2] = 'abc';                 //поменяли элемент в массиве под индексом 2
+console.log(myArray);
+myArray.push(7);  //добавили в конец массива
+console.log(myArray);
+myArray.pop();  //удалили последний элемент массива
+console.log(myArray);
+const removedElement =  myArray.pop(); //удалили последний элемент массива (это число 4) и вернули его в эту переменную
+console.log(myArray); //после объявления переменной removedElement, из myArray пропал элемент 4
+console.log(removedElement);
+myArray.unshift(5);  //добавили в начало массива элемент
+console.log(myArray);
+myArray.shift(5);  //удалили в начале массива элемент
+console.log(myArray);
+const myArrayNew = [1, 2, 3];
+myArrayNew.forEach(el => console.log(el*2)); //Перебрали все элементы массива, вызвали консоль кол-во раз сколько и массивов умножив на 2
+console.log(myArrayNew); //при этом оригинальный массив не изменился
+const res1 = myArrayNew.forEach(el => console.log(el*2)); //При этом в forEach нету return
+console.log(res1);  //Поэтому мы будем постоянно иметь undefined при объявлении в переменной
+const res2 = myArrayNew.map(el => el*4); //map тот же forEach но имеет return и собирает новый массив
+console.log(res2); //Видим что сформировался новый массив!!!!!
+console.log(myArrayNew) // оригинальный массив остался неизменным    */
+
+/* //Деструктуризация объектов
+  const userProfile = {
+      name: 'Maksim',
+      commentsQty: 23,
+      hasSignedAgreement: false
+  }
+const { name, commentsQty } = userProfile; //объявление новых переменных и присваивание значений на основе значений св-в объекта
+const { hasSignedAgreement } = userProfile;
+console.log(name);
+console.log(hasSignedAgreement);
+*/
+/*
+//Деструктуризация массивов
+ const fruits = ['Apple', 'Banana'];
+ const [fruitOne, fruitTwo] = fruits; //Объявление новых переменных и присваивание значений на основе элементов массива
+  console.log(fruitOne);
+  console.log(fruitTwo); */
